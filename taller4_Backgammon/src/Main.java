037113007b5590b104ac7ac340afab2d3b3b2916
dgior <giovannyduran2906@gmail.com>
2026-03-15 15:50:01 -0500

    se sube la clase main

---
+++ taller4_Backgammon/src/Main.java
@@ -0,0 +1,8 @@
+import backgammon.game.JuegoBackgammon;
+
+public class Main {
+    public static void main(String[] args) {
+        JuegoBackgammon juego = new JuegoBackgammon();
+        juego.jugar();
+    }
+}
